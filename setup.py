ea203385dbdec886e00a712a36183d34b1904cf6
rafi4482 <rafi.kazla@gmail.com>
2026-03-29 17:20:07 +0600

    Setup file updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='medical_chatbot',
+    version='0.1',
+    author='Rafi Uz Zaman',
+    author_email='rafi.kazla@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
+
